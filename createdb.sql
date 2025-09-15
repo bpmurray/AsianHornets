482355132ef849c79c91878319a3b42aed0c1e66
Brendan Murray <bpmurray@users.noreply.github.com>
2025-09-15 15:06:44 +0100

    Tweaked data types

--- createdb.sql
+++ createdb.sql
@@ -3,24 +3,30 @@ CREATE DATABASE IF NOT EXISTS ahtraps;
 USE ahtraps;
 CREATE OR REPLACE TABLE `owner` (
 	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-	`Name` VARCHAR(255) NOT NULL,
-	`email` VARCHAR(255) NOT NULL UNIQUE,
-	`phone` TEXT(16),
-	`password` TEXT(256),
+	`Name` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`phone` text DEFAULT NULL,
+	`password` text DEFAULT NULL,
 	PRIMARY KEY(`id`)
-) COMMENT='Contains details of individual owners';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci COMMENT='Contains details of individual owners';
+
+ALTER TABLE `owner`
+	ADD UNIQUE KEY `email` (`email`);
 
 CREATE OR REPLACE TABLE `trap` (
 	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
 	`full_id` TEXT(16) NOT NULL,
 	`traptype` TEXT(16) NOT NULL,
-	`owner` INTEGER,
-	`loc_lat` DECIMAL,
-	`loc_long` DECIMAL,
-	`loc_county` TEXT(16),
+	`owner` INTEGER DEFAULT NULL,
+	`loc_lat` DECIMAL(11,8) DEFAULT NULL,
+	`loc_long` DECIMAL(11,8) DEFAULT NULL,
+	`loc_county` TEXT(16) DEFAULT NULL,
 	PRIMARY KEY(`id`)
 ) COMMENT='Trap information';
 
 ALTER TABLE `trap`
-ADD FOREIGN KEY(`owner`) REFERENCES `owner`(`id`)
-ON UPDATE NO ACTION ON DELETE SET NULL;
+ALTER TABLE `trap`
+	ADD KEY `owner` (`owner`);
+	ADD FOREIGN KEY(`owner`) REFERENCES `owner`(`id`)
+	ON UPDATE NO ACTION ON DELETE SET NULL;
+
